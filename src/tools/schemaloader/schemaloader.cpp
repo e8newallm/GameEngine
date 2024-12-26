7f42e1aa0f1569d9c279de257cccfc47642d0feb
Matthew Newall <matthewnewall@hotmail.com>
2024-12-26 16:21:05 +0000

    Chang schemaloader to only modify a schema header if it would be changed to stop constant rebuilding

--- src/tools/schemaloader/schemaloader.cpp
+++ src/tools/schemaloader/schemaloader.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -16,12 +17,29 @@ int main(int argc, char *argv[])
         std::string path = argv[i];
         std::string schema = path.substr(path.find_last_of("/\\") + 1);
         schema = schema.substr(0, schema.find_last_of('.'));
-        std::ofstream header(std::string(argv[1]) + "/" + schema + ".h");
+
         std::ifstream schemaFile(path);
         std::stringstream buffer;
         buffer << schemaFile.rdbuf();
         schemaFile.close();
         data += "const char* " + std::string(schema) + " = R\"SCHEMA(\r\n" + buffer.str() + ")SCHEMA\";\r\n\r\n";
+
+        std::string headerFile = std::string(argv[1]) + "/" + schema + ".h";
+        if(std::filesystem::exists(headerFile))
+        {
+            std::ifstream existHeader(headerFile);
+            std::stringstream existBuffer;
+            existBuffer << existHeader.rdbuf();
+            existHeader.close();
+
+            if(existBuffer.str() == data)
+            {
+                continue;
+            }
+        }
+
+        std::fstream header(headerFile);
+        
         header << data;
         header.close();
     }
