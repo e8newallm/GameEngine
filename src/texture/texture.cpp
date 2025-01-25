be5db10896492ad3a8ea52ff9665141995490ce9
Matthew Newall <matthewnewall@hotmail.com>
2025-01-25 02:31:03 +0000

    Add surface conversion to uploadTexture

--- src/texture/texture.cpp
+++ src/texture/texture.cpp
@@ -1,4 +1,5 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_gpu.h>
 #include <string>
 
 #include "logging.h"
@@ -37,6 +38,8 @@ template <> Store<SDL_GPUTexture>::~Store()
 
 SDL_GPUTexture* uploadTexture(SDL_GPUDevice* gpu, SDL_Surface* surf, std::string filename)
 {
+    SDL_Surface* convertSurf = SDL_ConvertSurface(surf, SDL_PIXELFORMAT_ABGR8888);
+
     SDL_GPUCommandBuffer* uploadCmdBuf = SDL_AcquireGPUCommandBuffer(gpu);
     SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(uploadCmdBuf);
 
@@ -44,29 +47,24 @@ SDL_GPUTexture* uploadTexture(SDL_GPUDevice* gpu, SDL_Surface* surf, std::string
     SDL_zero(texInfo);
     texInfo.type = SDL_GPU_TEXTURETYPE_2D;
     texInfo.format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM;
-    texInfo.width = surf->w;
-    texInfo.height = surf->h;
+    texInfo.width = convertSurf->w;
+    texInfo.height = convertSurf->h;
     texInfo.layer_count_or_depth = 1;
     texInfo.num_levels = 1;
     texInfo.usage = SDL_GPU_TEXTUREUSAGE_SAMPLER;
     SDL_GPUTexture* tex = SDL_CreateGPUTexture(gpu, &texInfo);
     SDL_SetGPUTextureName(gpu, tex, filename.c_str());
-    Logger::message("SDL_SetGPUTextureName");
+
     SDL_GPUTransferBufferCreateInfo texBuffer;
     SDL_zero(texBuffer);
     texBuffer.usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD;
-    texBuffer.size = surf->w * surf->h * 4;
+    texBuffer.size = convertSurf->h * convertSurf->pitch;
     SDL_GPUTransferBuffer* textureTransferBuffer = SDL_CreateGPUTransferBuffer(gpu, &texBuffer);
-    Logger::message("textureTransferBuffer");
+
     void* textureTransferPtr = SDL_MapGPUTransferBuffer(gpu,
                                             textureTransferBuffer, false);
-    Logger::message("TEST " + std::to_string(surf->w * surf->h * 4));
-    Logger::message("Pointer " + std::to_string((long long)surf->pixels));
-    Logger::message("textureTransferPtr " + std::to_string((long long)textureTransferPtr));
-    SDL_memcpy(textureTransferPtr, surf->pixels, surf->w * surf->h);
-    Logger::message("TEST");
+    SDL_memcpy(textureTransferPtr, convertSurf->pixels, texBuffer.size);
     SDL_UnmapGPUTransferBuffer(gpu, textureTransferBuffer);
-    Logger::message("TEST");
 
     SDL_GPUTextureTransferInfo texTrans;
     SDL_zero(texTrans);
@@ -76,8 +74,8 @@ SDL_GPUTexture* uploadTexture(SDL_GPUDevice* gpu, SDL_Surface* surf, std::string
     SDL_GPUTextureRegion texRegion;
     SDL_zero(texRegion);
     texRegion.texture = tex;
-    texRegion.w = surf->w;
-    texRegion.h = surf->h;
+    texRegion.w = convertSurf->w;
+    texRegion.h = convertSurf->h;
     texRegion.d = 1;
     SDL_UploadToGPUTexture(copyPass, &texTrans, &texRegion, false);
 
